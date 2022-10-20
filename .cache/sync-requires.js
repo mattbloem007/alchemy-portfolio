c543719c6f76c2427f5e1f23610ef7640fb532fb
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2022-10-20 10:51:18 +0200

    new with gitignore

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/gabriel/Documents/alchemy-dark/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/gabriel/Documents/alchemy-dark/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/gabriel/Documents/alchemy-dark/src/pages/index.js"))),
   "component---src-template-archive-js": hot(preferDefault(require("/Users/gabriel/Documents/alchemy-dark/src/template/archive.js"))),
